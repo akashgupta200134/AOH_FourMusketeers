0c9846ade7d7469481bd59fcbb66e9a7e7d52b9f
akashgupta200134 <akashgupta_ce_2021@ltce.in>
2024-03-30 16:19:47 +0530

    bloodbank

--- arogya-sahayak/src/App.js
+++ arogya-sahayak/src/App.js
@@ -1,29 +1,29 @@
 import React from "react";
- //import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-// import { VideoRoom } from "./components/VideoRoom";
-// import Home from "./pages/Herosection";
-// import About from "./pages/About";
-// import Contact from "./pages/Contact";
-// import Doctors from "./pages/Doctors";
-// import Appointments from "./pages/Appointments";
-// import Services from "./pages/Services";
+import { VideoRoom } from "./components/VideoRoom";
+import Home from "./pages/Herosection";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Doctors from "./pages/Doctors";
+import Appointments from "./pages/Appointments";
+import Services from "./pages/Services";
 // import Signup from "./pages/Signup";
 // import Signin from "./pages/Signin";
-// import BloodBank from "./components/BloodBank";
-// import ChatBot from "./pages/ChatBot";
-// import { StateProvider } from "./context/StateProvider";
-// import reducer from "./context/reducer";
-// import { initialState } from "./context/initialState";
+import BloodBank from "./components/BloodBank";
+import ChatBot from "./pages/ChatBot";
+import { StateProvider } from "./context/StateProvider";
+import reducer from "./context/reducer";
+import { initialState } from "./context/initialState";
 // import Register from "./pages/Register";
-// import Footer from "./pages/Footer";
+import Footer from "./pages/Footer";
 // import Department from "./pages/Department";
 // import Reviews from "./pages/Reviews";
-// import Blogs from "./pages/Blogs";
-// import AllPages from "./pages/AllPages"
-// import "./App.css";
-// import Profile from "./pages/Profile";
-// import { dataLoader } from "./utils/firebaseFunctions";
+import Blogs from "./pages/Blogs";
+import AllPages from "./pages/AllPages"
+import "./App.css";
+import Profile from "./pages/Profile";
+import { dataLoader } from "./utils/firebaseFunctions";
 // import BookSlot from "./pages/BookSlot";
 // import DoctorProfile from "./pages/DoctorProfile";
 // import Pricingplans from "./pages/Pricingplans";
@@ -40,6 +40,45 @@ function App() {
   
     <div className="w-screen h-auto flex flex-col bg-primary">
     <Navbar />
+   
+    <main className="mt-14 md:mt-20 px-4 md:px-16 py-4 w-full">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          {/* <Route path="/contact" element={<Contact />} />
+          <Route path="/doctors" element={<Doctors />} /> */}
+          <Route path="/appointments" element={<Appointments />} />
+          <Route path="/allpages" element={<AllPages />} />
+          {/* <Route path="/ChatBot" element={<ChatBot />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} /> */}
+          <Route path="/register" element={<Register />} />
+          <Route path="/videocall" element={<VideoRoom />} />
+          <Route path="/bloodbank" element={<BloodBank />} />
+          {/* <Route path="/profile" element={<Profile />} />
+          <Route path="/bookslots/:docID" element={<BookSlot />} />
+          <Route path="/department" element={<Department />} />
+          <Route path="/departmentdetails" element={<Departmentdetails />} />
+          <Route path="/reviews" element={<Reviews />} />
+          <Route path="/blogs" element={<Blogs />} /> */}
+          {/* <Route path="/doctors/:docID" element={<DoctorProfile />} />
+          <Route path="/pricingplans" element={<Pricingplans />} />
+          <Route path="/gallery" element={<Gallery/>} />
+          <Route path="/timetable" element={<Timetable />} />
+          <Route path="/doctorRegister" element={<DoctorRegister />} />
+          <Route path="/newsignup" element={<NewSignUp />} />
+          <Route path="/search" element={<SearchPatient />} /> */}
+          
+
+
+        </Routes>
+      </main>
+
+      <Footer />
+
+
+
 
 
 
--- arogya-sahayak/src/components/BloodBank.jsx
+++ arogya-sahayak/src/components/BloodBank.jsx
@@ -1,5 +1,9 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { getAllBloodRecords, saveBloodRecords } from "../utils/firebaseFunctions";
+import { useStateValue } from "../context/StateProvider";
+import UpdateBloodRecordForm from "./UpdateBloodRecordForm";
+import { data } from "../utils/sampleData";
+import Bloodhero from '../images/blood3.png'
 
 
 
@@ -22,10 +26,80 @@ import React from 'react'
     }, []);
   
 
-  return (
- 
-  )
-}
+    return (
+      <div className="mt-4">
+        <section className="flex items-center justify-center w-full md:pl-36 -mt-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-2 max-w-6xl" id="home">
+            <div className="md:h-screen flex flex-col justify-center md:pb-36 pt-10 md:pt-0 md:gap-3 gap-1">
+              <div className="flex"></div>
+              <p className="md:text-7xl text-4xl font-extrabold mt-5">Give Blood, Save Lives&nbsp;</p>
+              <p className="mt-5 md:mt-1 text-mediumGrey md:w-3/4 text-xl">
+                We are committed to providing you with the best medical and healthcare services to help you live healthier and happier.
+              </p>
+            </div>
+            <div className="relative">
+              <div className="md:h-screen md:absolute flex items-center justify-center md:pb-36 h-96 w-[400px] mt-10">
+                <img src={Bloodhero} alt="Hero" />
+              </div>
+            </div>
+          </div>
+        </section>
+  
+        <div className="-mt-24">
+          <div className="flex flex-row gap-7 justify-center">
+            <button className="bg-blue-500  hover:bg-blue-800 h-10 w-36 text-white text-xl font-semibold rounded-3xl shadow-2xl text-center" onClick={() => saveBloodRecords(data)}>Add Record</button>
+            <button className="bg-blue-500  hover:bg-blue-800 h-10 w-48 text-white text-xl font-semibold rounded-3xl shadow-2xl text-center" onClick={getAllBloodRecords}>Log Blood Records</button>
+          </div>
+  
+          <h2 className="text-5xl font-extrabold text-center mt-16">Blood Records</h2>
+          <div className="flex flex-row justify-center mt-10 overflow-x-auto">
+            <table className="border-collapse border border-gray-800 rounded-md ">
+              <thead>
+                <tr className="bg-blue-400 text-white  text-lg font-semibold">
+                  <th className="border border-gray-800 px-4 py-2">Blood Type</th>
+                  <th className="border border-gray-800 px-4 py-2">Quantity</th>
+                  <th className="border border-gray-800 px-4 py-2">Availability</th>
+                  <th className="border border-gray-800 px-4 py-2">Price</th>
+                  <th className="border border-gray-800 px-4 py-2">Last Update</th>
+                </tr>
+              </thead>
+              <tbody>
+                {allBloodRecords.map(hospital =>
+                  hospital.bloodRecords.map((record, index) => (
+                    <tr key={`${hospital.adminEmail}-${record.bloodType}-${index}`} className=" text-md  font-medium">
+                           {/* {index === 0 && (
+                      <td rowSpan={hospital.bloodRecords.length}>
+                        {hospital.host_name}
+                      </td>
+                    )} */}
+                    {/* {index === 0 && (
+                      <td rowSpan={hospital.bloodRecords.length}>
+                        {hospital.adminEmail}
+                      </td>
+                    )} */}
+                      <td className="border border-gray-800 px-4 py-2">{record.bloodType}</td>
+                      <td className="border border-gray-800 px-4 py-2">{record.quantity}</td>
+                      <td className="border border-gray-800 px-4 py-2">{record.availability}</td>
+                      <td className="border border-gray-800 px-4 py-2">{record.price}</td>
+                        {/* {index === 0 && (
+                      <td rowSpan={hospital.bloodRecords.length}>
+                        {hospital.last_update}
+                      </td>
+                    )} */}
+                      <td className="border border-gray-800 px-4 py-2">{hospital.last_update}</td>
+                    </tr>
+                  ))
+                )}
+              </tbody>
+            </table>
+          </div>
+  
+          {adminEmailData && <UpdateBloodRecordForm data={adminEmailData} />}
+        </div>
+      </div>
+    );
+  };
+  
+  export default HospitalList;
+  
 
-
-export default BloodBank
