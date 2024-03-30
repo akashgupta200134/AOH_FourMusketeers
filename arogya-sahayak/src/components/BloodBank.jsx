ed3d9103daf16d57da0db9f7863de82d0e02756c
akashgupta200134 <akashgupta_ce_2021@ltce.in>
2024-03-30 16:07:27 +0530

    bloodbank

--- arogya-sahayak/src/components/BloodBank.jsx
+++ arogya-sahayak/src/components/BloodBank.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 
-const BloodBank = () => {
+
+
+
+  const HospitalList = () => {
+    const [allBloodRecords, setAllBloodRecords] = useState([]);
+    const [{ user }, dispatch] = useStateValue();
+    const [adminEmailData, setAdminEmailData] = useState();
+  
+    const fetchallRecords = async () => {
+      const data = await getAllBloodRecords();
+      setAllBloodRecords(data);
+      if (user.email) {
+        const filteredData = data.filter(record => record.adminEmail === user.email);
+        setAdminEmailData(filteredData[0]);
+      }
+    };
+
+    useEffect(() => {
+      fetchallRecords();
+    }, []);
+  
+
   return (
-    <div>BloodBank</div>
+ 
   )
 }
 
